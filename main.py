e56c548389b5790b66ffcef6447cc38a84a0c333
liam-gen <74251382+liam-gen@users.noreply.github.com>
2021-03-26 14:33:24 +0100

    Maj 0.0.3 | 26/03/2021

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ## Installer la package 
 from __init__ import bot
 
-## Créer le bot : bot("prefix", "description", "token", "status", couleur)
-## La couleur ne dois pas etre entre "" 
+## Créer le bot : bot("prefix", "description", "token", "status", couleur, id_salon_arrivé_départ)
+## La couleur et l'id du salon d'arivée et de départ ne dois pas etre entre "" 
 bot()

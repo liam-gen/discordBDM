4784f3d1503b17a6203a84afb0cde2866fc96aaa
CodeElevator <98259474+CodeElevator@users.noreply.github.com>
2022-02-09 18:51:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ## Installer la package 
-from __init__ import bot
+from __init__ import Bot
 
 ## Créer le bot : bot("prefix", "description", "token", "status", couleur, id_salon_arrivé_départ)
 ## La couleur et l'id du salon d'arivée et de départ ne dois pas etre entre "" 
-bot()
+Bot()
